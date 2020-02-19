3f59385f4df3f9ff2fe48c7639e850f9b6b6d303
zuoli <zli119@stevens.edu>
2020-02-18 22:46:32 -0500

    create readme, create ui

---
+++ README.md
@@ -0,0 +1,22 @@
+# Document_Classify
+# A program that reads documents of every kind. Gueses what the contents are and creates and index of documents.
+
+### 1. target
+
+A local program that reads documents of every kind. Gueses what the contents are and creates and index of documents.
+(1) Open a folder, it will classify the documents automatically
+(2) store the class labels into database, according to the file name (every documents may have several lables)
+
+### 2. technology
+
+* Java  
+* Java Swing
+
+### 3. function
+
+* Read document -> count key words -> ranking -> decide documents label
+
+* Create folders -> classify documents to different folders
+
+### 4. process
+(1) create a program that can do statistics of words in a document.
---
+++ src/com/company/ui/UI.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company.ui;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+
+/**
+ *
+ * @author Administrator
+ */
+public class UI extends JFrame{
+        private FileReader fileReader;
+        private BufferedReader bufferedReader;
+        private FileWriter fileWriter;
+        private BufferedWriter bufferedWriter;
+    public UI(){
+        super("TextEditor");
+        setSize(400,500);
+        setLocation(500,100);
+        Container c=getContentPane();
+        c.setBackground(Color.BLUE);
+        JMenu m1=new JMenu("File");
+        JMenuItem mi1=new JMenuItem("New");
+        JMenuItem mi2=new JMenuItem("Save");
+        JMenuItem mi3=new JMenuItem("Open");
+        JMenuItem mi4=new JMenuItem("Quit");
+        m1.add(mi1);
+        m1.add(mi3);
+        m1.add(mi2);        
+        m1.add(mi4);        
+        JMenu m2=new JMenu("Build");
+        JMenuItem mi5=new JMenuItem("Compile");
+        JMenuItem mi6=new JMenuItem("Run");
+        m2.add(mi5);
+        m2.add(mi6);
+        JMenuBar mb=new JMenuBar();
+        mb.add(m1);
+        mb.add(m2);
+        c.add(mb,BorderLayout.NORTH);
+        JTextArea ta=new JTextArea();
+        c.add(ta,BorderLayout.CENTER);
+        setVisible(true);
+        mi1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                ta.setText("");
+            }
+        });
+        mi2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                JFileChooser fc=new JFileChooser();
+                fc.showSaveDialog(null);
+                fc.setVisible(true);
+                try {                      			
+                    BufferedWriter bw=new BufferedWriter(new FileWriter(fc.getSelectedFile().getAbsolutePath()));
+                    bw.write(ta.getText());           
+                } catch (IOException ex) {
+                    Logger.getLogger(UI.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        mi3.addActionListener(new ActionListener(){        
+            public void actionPerformed(ActionEvent e){
+                JFileChooser fc=new JFileChooser();
+                fc.showOpenDialog(fc);
+                try {
+                    BufferedReader br=new BufferedReader(new FileReader(fc.getSelectedFile().getAbsolutePath()));                    
+                    String getAll="";
+                    while((br.readLine())!=null){
+                    getAll+=br.readLine()+"\r\n";
+                }                    
+                    ta.setText(getAll);
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(UI.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(UI.class.getName()).log(Level.SEVERE, null, ex);
+                }		
+		}
+        });
+         mi4.addActionListener(new ActionListener(){
+             public void actionPerformed(ActionEvent e){
+                 int exitChoose=JOptionPane.showConfirmDialog(ta,"Are you sure to close?","Close Tip",JOptionPane.OK_CANCEL_OPTION);
+			if(exitChoose==JOptionPane.OK_OPTION)
+			{	System.exit(0);
+			}
+			else
+			{	return;
+			}
+             }
+         });
+         mi5.addActionListener(new ActionListener(){
+             public void actionPerformed(ActionEvent e){
+                Runtime r=Runtime.getRuntime();
+                 try {
+                     Process p=r.exec("test.java");                     
+                 } catch (IOException ex) {
+                     Logger.getLogger(UI.class.getName()).log(Level.SEVERE, null, ex);
+                 }
+             }
+         });
+         mi6.addActionListener(new ActionListener(){
+             public void actionPerformed(ActionEvent e){
+                 Runtime r=Runtime.getRuntime();
+                 try {
+                     Process p=r.exec("javac test.java");                     
+                 } catch (IOException ex) {
+                     Logger.getLogger(UI.class.getName()).log(Level.SEVERE, null, ex);
+                 }
+             }
+         });        
+            File f=new File("test.java");
+            long t=f.lastModified();       
+    }
+    public static void main(String[] args){
+        new UI();
+        
+        
+    }
+    
+}
