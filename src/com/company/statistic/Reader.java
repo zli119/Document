5d09954d8732eaf9c7cebefca740f22d8f8723c6
zuoli <zli119@stevens.edu>
2020-02-20 20:46:41 -0500

    create data set, compare the keywords set and data set, to figure out the class(use HashMap)

--- src/com/company/statistic/Reader.java
+++ src/com/company/statistic/Reader.java
@@ -6,6 +6,8 @@ import java.util.*;
 
 public class Reader {
     private static Set<String> voidWords;
+    private static List<Set<String>> setList;
+
     // Builder void words set
     public static void voidWordsBuilder() throws FileNotFoundException, IOException {
         voidWords = new HashSet<>();
@@ -18,8 +20,8 @@ public class Reader {
         br.close();
     }
 
-    // build words number map, then ranking words
-    public static void countWords() throws FileNotFoundException, IOException{
+    // build words number map
+    public static HashMap<String, Integer> countWords() throws FileNotFoundException, IOException{
         HashMap <String, Integer> wordCounter;
         BufferedReader br=new BufferedReader(new FileReader(".txt"));
         String s="";
@@ -33,11 +35,51 @@ public class Reader {
             }
         }
         br.close();
+        return wordCounter;
     }
+
+    // create data set, compare the keywords set and data set, to figure out the class(use HashMap)
+    public static Set top20Keyword(HashMap<String, Integer> map) {
+        Data[] data = new Data[map.size()];
+        int i = 0;
+        for (String key : map.keySet()) {
+            data[i].word = key;
+            data[i].count = map.get(key);
+            i++;
+        }
+        Arrays.sort(data, (a, b) -> Integer.compare(b.count, a.count));
+        Set<String> set = new HashSet<>();
+        i = 0;
+        while (i < 10 && i < data.length) {
+            set.add(data[i].word);
+        }
+        return set;
+    }
+
+    // compare set
+    public Set compareSet(Set input) {
+        int maxIndex = 0;
+        int size = 0;
+        for (int i = 0; i < setList.size(); i++) {
+            Set t = new HashSet(input);
+            t.retainAll(setList.get(i));
+            if (size < t.size()) {
+                size = t.size();
+                maxIndex = i;
+            }
+        }
+        return setList.get(maxIndex);
+    }
+
+
     public static void main(String[] args) throws IOException{
         String input="";
         voidWordsBuilder();
         System.out.println(voidWords.size());
     }
 
+}
+class Data{
+    int count;
+    String word;
 }
